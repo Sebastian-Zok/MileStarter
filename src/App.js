801b9bac9e5b6e57fb600c938d9ac77e1b3bb1a8
Sebastian-Zok <sebastianpaulzok@gmail.com>
2023-03-22 15:16:16 +0100

    Connect to Wallet Button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App extends Component {
   componentWillMount() {
     this.loadBlockchainData()
   }
+  
+    constructor(props) {
+      super(props)
+      this.state = { account: '' }
+    }
 
   async loadBlockchainData() {
     const web3 = new Web3(Web3.givenProvider)
@@ -13,15 +18,21 @@ class App extends Component {
     this.setState({ account: accounts[0] })
   }
 
-  constructor(props) {
-    super(props)
-    this.state = { account: '' }
+  async connect() {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      window.web3 = new Web3(window.ethereum);
+    } else {
+      console.log("No wallet");
+    }
   }
+
 
   render() {
     return (
       <div className="container">
         <p>Your address: {this.state.account}</p>
+        <button onClick={this.connect}>Connect</button>
       </div>
     );
   }
